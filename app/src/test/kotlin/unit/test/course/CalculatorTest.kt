98cd6951f6f34e9b0f830502d239890987115900
imufiid <imufiid98@gmail.com>
2021-07-05 09:45:05 +0700

    implement display name generator

--- app/src/test/kotlin/unit/test/course/CalculatorTest.kt
+++ app/src/test/kotlin/unit/test/course/CalculatorTest.kt
@@ -8,10 +8,13 @@ package unit.test.course
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.assertThrows
+import unit.test.course.generator.SimpleDisplayNameGenerator
 import java.lang.IllegalArgumentException
 
-@DisplayName("Test for Calculator Class")
+@DisplayNameGeneration(SimpleDisplayNameGenerator::class)
+//@DisplayName("Test for Calculator Class")
 internal class CalculatorTest {
     private val firstNum = 5
     private val secondNum = 1
@@ -19,7 +22,7 @@ internal class CalculatorTest {
     private var calculator = Calculator()
 
     @Test
-    @DisplayName("Test for function calculator.add(5, 1)")
+//    @DisplayName("Test for function calculator.add(5, 1)")
     fun testAddSuccess() {
         val result = calculator.add(firstNum, secondNum)
         assertNotNull(result)
@@ -27,7 +30,7 @@ internal class CalculatorTest {
     }
 
     @Test
-    @DisplayName("Test for function calculator.divide(5, 1)")
+//    @DisplayName("Test for function calculator.divide(5, 1)")
     fun testDivideSuccess() {
         val result = calculator.divide(firstNum, secondNum)
         assertNotNull(result)
@@ -35,7 +38,7 @@ internal class CalculatorTest {
     }
 
     @Test
-    @DisplayName("Test throw error for function calculator.divide(5, 1)")
+//    @DisplayName("Test throw error for function calculator.divide(5, 1)")
     fun testDivideFailed() {
         assertThrows<IllegalArgumentException> {
             calculator.divide(firstNum, 0)
