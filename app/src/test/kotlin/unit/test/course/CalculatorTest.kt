aa756e97d8b9cb2122a06dff5c79525c5722798a
imufiid <imufiid98@gmail.com>
2021-07-05 09:50:13 +0700

    implement disabled test

--- app/src/test/kotlin/unit/test/course/CalculatorTest.kt
+++ app/src/test/kotlin/unit/test/course/CalculatorTest.kt
@@ -5,16 +5,12 @@
 
 package unit.test.course
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.DisplayNameGeneration
-import org.junit.jupiter.api.assertThrows
-import unit.test.course.generator.SimpleDisplayNameGenerator
 import java.lang.IllegalArgumentException
 
-@DisplayNameGeneration(SimpleDisplayNameGenerator::class)
-//@DisplayName("Test for Calculator Class")
+//@DisplayNameGeneration(SimpleDisplayNameGenerator::class)
+@DisplayName("Test for Calculator Class")
 internal class CalculatorTest {
     private val firstNum = 5
     private val secondNum = 1
@@ -22,7 +18,7 @@ internal class CalculatorTest {
     private var calculator = Calculator()
 
     @Test
-//    @DisplayName("Test for function calculator.add(5, 1)")
+    @DisplayName("Test for function calculator.add(5, 1)")
     fun testAddSuccess() {
         val result = calculator.add(firstNum, secondNum)
         assertNotNull(result)
@@ -30,7 +26,7 @@ internal class CalculatorTest {
     }
 
     @Test
-//    @DisplayName("Test for function calculator.divide(5, 1)")
+    @DisplayName("Test for function calculator.divide(5, 1)")
     fun testDivideSuccess() {
         val result = calculator.divide(firstNum, secondNum)
         assertNotNull(result)
@@ -38,12 +34,17 @@ internal class CalculatorTest {
     }
 
     @Test
-//    @DisplayName("Test throw error for function calculator.divide(5, 1)")
+    @DisplayName("Test throw error for function calculator.divide(5, 1)")
     fun testDivideFailed() {
         assertThrows<IllegalArgumentException> {
             calculator.divide(firstNum, 0)
         }
     }
 
+    @Test
+    @Disabled("Coming Soon")
+    fun testComingSoon() {
+        assertTrue(true)
+    }
 
 }
