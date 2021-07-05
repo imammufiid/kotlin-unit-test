fc64a691c3fc1a315ee0e5c9761299da1f488fc8
imufiid <imufiid98@gmail.com>
2021-07-05 09:20:47 +0700

    create calculator class

---
+++ app/src/main/kotlin/unit/test/course/Calculator.kt
@@ -0,0 +1,10 @@
+/*
+ * Created on 7/4/21, 8:24 PM
+ * Copyright (c) Imam Mufiid 2021. All right reserved.
+ */
+
+package unit.test.course
+
+class Calculator {
+    fun add(first: Int, second: Int): Int = first + second
+}
