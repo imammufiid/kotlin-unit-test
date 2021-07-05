daf066ef7c4f885bbffaa76ddc5fb704eeede395
imufiid <imufiid98@gmail.com>
2021-07-05 09:21:11 +0700

    create test with assertion

---
+++ app/src/test/kotlin/unit/test/course/CalculatorTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Created on 7/4/21, 8:25 PM
+ * Copyright (c) Imam Mufiid 2021. All right reserved.
+ */
+
+package unit.test.course
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+internal class CalculatorTest {
+    private val firstNum = 1
+    private val secondNum = 5
+    private val resultNum = 6
+    private var calculator = Calculator()
+
+    @Test
+    fun testAddSuccess() {
+        val result = calculator.add(firstNum, secondNum)
+        assertNotNull(result)
+        assertEquals(resultNum, result, "Hasil seharusnya $resultNum")
+    }
+
+    @Test
+    fun testAddSuccessNotEqual() {
+        val result = calculator.add(firstNum, secondNum)
+        assertNotNull(result)
+        assertNotEquals(5, result)
+    }
+}
